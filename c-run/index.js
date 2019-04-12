d608f17fa182b31b7a043361cf3d8b8083732851
koba1108 <paseli.koba@gmail.com>
2019-04-12 20:11:23 +0900

    fix

--- c-run/index.js
+++ c-run/index.js
@@ -1,10 +1,38 @@
 const express = require('express')
 const app = express()
 
+const checkPath = (req) => {
+  console.log('originalUrl', req.originalUrl) // '/admin/new'
+  console.log('originalUrl', req.baseUrl) // '/admin'
+  console.log('originalUrl', req.path) // '/new'
+}
+
 app.get('/', (req, res) => {
+  checkPath(req)
   console.log('Hello world received a request.')
   const target = process.env.TARGET || 'World'
-  res.send(`Hello ${target}!`)
+  res.send(`1 Hello ${target}!`)
+})
+
+app.get('/helloworld', (req, res) => {
+  checkPath(req)
+  console.log('Hello world received a request.')
+  const target = process.env.TARGET || 'World'
+  res.send(`2 Hello ${target}!`)
+})
+
+app.get('/helloworld/**', (req, res) => {
+  checkPath(req)
+  console.log('Hello world received a request.')
+  const target = process.env.TARGET || 'World'
+  res.send(`3 Hello ${target}!`)
+})
+
+app.get('/**', (req, res) => {
+  checkPath(req)
+  console.log('Hello world received a request.')
+  const target = process.env.TARGET || 'World'
+  res.send(`4 Hello ${target}!`)
 })
 
 const port = process.env.PORT || 8080
